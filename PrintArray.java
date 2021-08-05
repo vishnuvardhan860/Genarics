ee1d9d3d00af3227e87a7661f765f7cb7874e93c
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-08-05 17:26:06 +0530

    Added:Strings_Maximum

---
+++ PrintArray.java
@@ -0,0 +1,33 @@
+package com.generics;
+
+public class PrintArray {
+	 public static void toPrint(Integer[] inputArray) {
+	        for (int element : inputArray) {
+	            System.out.printf("s", element);
+	        }
+	        System.out.println();
+	    }
+
+	    public static void toPrint(Double[] inputArray) {
+	        for (double element : inputArray) {
+	            System.out.printf("s", element);
+	        }
+	        System.out.println();
+	    }
+
+	    public static void toPrint(Character[] inputArray) {
+	        for (char element : inputArray) {
+	            System.out.printf("s", element);
+	        }
+	        System.out.println();
+	    }
+
+	    public static void main(String[] args) {
+	        Integer [] intArray = {1,2,3,4,5};
+	        Double  [] doubleArray ={1.1,2.2,3.3,4.4,5.5};
+	        Character [] charArray ={'h','e','f','e'};
+	        PrintArray.toPrint(intArray);
+	        PrintArray.toPrint(doubleArray);
+	        PrintArray.toPrint(charArray);
+	    }
+	}
